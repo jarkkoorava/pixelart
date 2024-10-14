ce268988f00735be36d5843c36e264d12b4664ab
Jarkko Orava <jarkko.orava@gmail.com>
2024-10-14 14:28:51 +0300

    add color selection to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import "./App.css";
 import Content from "./Content";
 import Header from "./Header";
+import { useState } from "react";
+
+interface color {
+  id: string | null;
+  hex: string;
+  name: string;
+}
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState<color>({
+    id: null, 
+    hex: "#000000",
+    name: "Black"
+  });
+
+  const changeSelectedColor = (newColor: color) => {
+    setSelectedColor(newColor);
+  }
+
   return (
     <div className="app">
       <div className="container">
         <Header />
-        <Content />
+        <Content selectedColor={selectedColor} changeSelectedColor={changeSelectedColor}/>
       </div>
     </div>
   );
