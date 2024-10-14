7456e8c4a48b2bbcc85fc0c17809065e366508cd
Jarkko Orava <jarkko.orava@gmail.com>
2024-10-14 16:05:35 +0300

    create basic drawing functionality

--- src/DrawingArea.tsx
+++ src/DrawingArea.tsx
@@ -1,14 +1,44 @@
-const DrawingArea = () => {
-  return (
-    <div className="drawing-area">
-      <h2>
-        Drawing area
-      </h2>
-      <div className="drawing-area-canvas-container">
-        <div className="drawing-area-canvas"></div>
-      </div>
-    </div>
-  )
+import React, { RefObject } from "react";
+
+interface color {
+  id: string | null;
+  hex: string;
+  name: string;
+}
+interface props {
+  selectedColor: color;
+  canvasRef: RefObject<HTMLCanvasElement>;
+  onMouseDown: (e: React.MouseEvent<HTMLCanvasElement>) => void;
+  onMouseUp: () => void;
+  onMouseMove: (e: React.MouseEvent<HTMLCanvasElement>) => void;
+  onMouseLeave: () => void;
 }
 
-export default DrawingArea
+const DrawingArea = ({
+  canvasRef,
+  onMouseDown,
+  onMouseUp,
+  onMouseMove,
+  onMouseLeave,
+}: props) => {
+  return (
+    <div className="drawing-area">
+      <h2>Drawing area</h2>
+      <div className="drawing-area-canvas-container">
+        <div className="drawing-area-canvas">
+          <canvas className="canvas"
+            ref={canvasRef}
+            width={16}
+            height={16}
+            onMouseDown={onMouseDown}
+            onMouseUp={onMouseUp}
+            onMouseMove={onMouseMove}
+            onMouseLeave={onMouseLeave}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DrawingArea;
